8d59a718044243a914909c0298e47572ce7b7e5f
mkunz-intension <106534528+mkunz-intension@users.noreply.github.com>
2022-09-09 10:22:30 +0200

    Update keycloak version to quarkus18 (#29)
    * FWUV-78 - Upgrade FWU extensions project to Keycloak 18
    
    * FWUV-78 - context path set to "/auth/" for KeycloakContainer test
    
    * FWUV-78 - "Sign out" button renamed for selenium tests

--- remove-user-on-logout/src/test/java/de/intension/listener/RemoveUserOnLogOutEventIT.java
+++ remove-user-on-logout/src/test/java/de/intension/listener/RemoveUserOnLogOutEventIT.java
@@ -22,7 +22,6 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
-import org.testcontainers.utility.MountableFile;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 
@@ -33,13 +32,12 @@ class RemoveUserOnLogOutEventIT
 
     private static Network             network           = Network.newNetwork();
 
-    private static KeycloakContainer   keycloak          = new KeycloakContainer("quay.io/keycloak/keycloak:16.1.1")
-        .withExtensionClassesFrom("target/classes")
+    private static KeycloakContainer   keycloak          = new KeycloakContainer("quay.io/keycloak/keycloak:18.0.2")
+        .withProviderClassesFrom("target/classes")
         .withNetwork(network)
         .withNetworkAliases("test")
-        .withEnv("KEYCLOAK_IMPORT", "/opt/keycloak/imports/fwu-realm.json,/opt/keycloak/imports/idp-realm.json")
-        .withCopyFileToContainer(MountableFile.forClasspathResource("fwu-realm.json"), "/opt/keycloak/imports/")
-        .withCopyFileToContainer(MountableFile.forClasspathResource("idp-realm.json"), "/opt/keycloak/imports/")
+        .withRealmImportFiles("/fwu-realm.json", "/idp-realm.json")
+        .withContextPath("/auth/")
         .withAccessToHost(true);
 
     private static GenericContainer<?> firefoxStandalone = new GenericContainer<>(DockerImageName.parse("selenium/standalone-firefox:4.3.0-20220706"))
@@ -205,7 +203,7 @@ class RemoveUserOnLogOutEventIT
 
         public KeycloakPage logout()
         {
-            String signOutButton = "Sign Out";
+            String signOutButton = "Sign out";
             wait.until(ExpectedConditions.elementToBeClickable(By.linkText(signOutButton)));
             driver.findElement(By.linkText(signOutButton)).click();
             return this;
