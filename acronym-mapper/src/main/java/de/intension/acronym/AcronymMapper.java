6314a3b3e46bf005c52f1f1dd8ef15dee999d5b8
Ingo Kuba <ikuba@intension.de>
2022-11-22 13:54:58 +0100

    other mapper

--- acronym-mapper/src/main/java/de/intension/acronym/AcronymMapper.java
+++ acronym-mapper/src/main/java/de/intension/acronym/AcronymMapper.java
@@ -1,45 +1,95 @@
 package de.intension.acronym;
 
-import org.keycloak.component.ComponentModel;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
-import org.keycloak.storage.ldap.LDAPStorageProvider;
-import org.keycloak.storage.ldap.idm.model.LDAPObject;
-import org.keycloak.storage.ldap.idm.query.internal.LDAPQuery;
-import org.keycloak.storage.ldap.mappers.AbstractLDAPStorageMapper;
+import org.keycloak.Config;
+import org.keycloak.broker.provider.AbstractIdentityProviderMapper;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+import org.keycloak.models.*;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * LDAP mapper that will combine the first two letters of the first and last name to a lowercase acronym.
+ * Identity provider mapper that will combine the first two letters of the first and last name to a lowercase acronym.
  */
-public class AcronymMapper extends AbstractLDAPStorageMapper {
+public class AcronymMapper extends AbstractIdentityProviderMapper {
 
-    public static final String USER_MODEL_ATTRIBUTE = "user.model.attribute";
+    public static final String PROVIDER_ID = "acronym-idp-mapper";
+    public static final String[] COMPATIBLE_PROVIDERS = {ANY_PROVIDER};
 
-    public AcronymMapper(ComponentModel mapperModel, LDAPStorageProvider ldapProvider) {
-        super(mapperModel, ldapProvider);
+    public static final String ATTRIBUTE = "attribute";
+    private static final String ATTRIBUTE_DEFAULT_VALUE = "acronym";
+
+    protected static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        ProviderConfigProperty property;
+        property = new ProviderConfigProperty();
+        property.setName(ATTRIBUTE);
+        property.setLabel("User Attribute");
+        property.setHelpText("Name of the user attribute, which will contain the acronym");
+        property.setType(ProviderConfigProperty.STRING_TYPE);
+        property.setDefaultValue(ATTRIBUTE_DEFAULT_VALUE);
+        configProperties.add(property);
+    }
+
+    private String attribute = ATTRIBUTE_DEFAULT_VALUE;
+
+    @Override
+    public boolean supportsSyncMode(IdentityProviderSyncMode syncMode) {
+        // supports any sync mode
+        return true;
     }
 
     @Override
-    public void onImportUserFromLDAP(LDAPObject ldapUser, UserModel user, RealmModel realm, boolean b) {
+    public String getHelpText() {
+        return "This mapper will combine the first two letters of the first and last name to a lowercase acronym.";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public String[] getCompatibleProviders() {
+        return COMPATIBLE_PROVIDERS;
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Attribute Importer";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Acronym";
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        this.attribute = config.get(ATTRIBUTE, ATTRIBUTE_DEFAULT_VALUE);
+    }
+
+    @Override
+    public void importNewUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        updateUser(user);
+    }
+
+    @Override
+    public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        updateUser(user);
+    }
+
+    private void updateUser(UserModel user) {
         var firstName = user.getAttributeStream(UserModel.FIRST_NAME).findFirst().orElse("");
         var lastName = user.getAttributeStream(UserModel.LAST_NAME).findFirst().orElse("");
 
-        user.setSingleAttribute(mapperModel.getConfig().getFirst(USER_MODEL_ATTRIBUTE),
-                AcronymUtil.createAcronym(firstName, lastName));
-    }
-
-    @Override
-    public void onRegisterUserToLDAP(LDAPObject ldapUser, UserModel user, RealmModel realm) {
-
-    }
-
-    @Override
-    public UserModel proxy(LDAPObject ldapUser, UserModel user, RealmModel realm) {
-        return user;
-    }
-
-    @Override
-    public void beforeLDAPQuery(LDAPQuery ldapQuery) {
-
+        user.setSingleAttribute(attribute, AcronymUtil.createAcronym(firstName, lastName));
     }
 }
