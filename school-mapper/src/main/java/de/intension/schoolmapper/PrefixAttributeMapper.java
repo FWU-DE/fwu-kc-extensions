1432be16445090295abd6e1afbb7b3291f9b04b2
Ingo Kuba <ikuba@intension.de>
2022-11-23 09:05:47 +0100

    TEMP: Prefixed mapper

--- school-mapper/src/main/java/de/intension/schoolmapper/PrefixAttributeMapper.java
+++ school-mapper/src/main/java/de/intension/schoolmapper/PrefixAttributeMapper.java
@@ -1,7 +1,6 @@
 package de.intension.schoolmapper;
 
 import org.jboss.logging.Logger;
-import org.keycloak.Config;
 import org.keycloak.broker.oidc.mappers.AbstractClaimMapper;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.models.*;
@@ -9,7 +8,7 @@ import org.keycloak.provider.ProviderConfigProperty;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PrefixAttributeMapper extends AbstractClaimMapper {
     public static final String PROVIDER_ID = "prefixed-attribute-idp-mapper";
@@ -17,13 +16,9 @@ public class PrefixAttributeMapper extends AbstractClaimMapper {
 
     protected static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
 
-    private String claim;
     public static final String ATTRIBUTE = "attribute";
-    private String userAttribute;
     public static final String PREFIX = "prefix";
-    private String prefix;
     public static final String LOWER_CASE = "toLowerCase";
-    private boolean toLowerCase = false;
 
     private final Logger logger = Logger.getLogger(PrefixAttributeMapper.class);
 
@@ -40,16 +35,16 @@ public class PrefixAttributeMapper extends AbstractClaimMapper {
         property.setLabel("Prefix");
         property.setHelpText("Prefix to add to the claim.");
         property.setType(ProviderConfigProperty.STRING_TYPE);
-
         configProperties.add(property);
+
         property = new ProviderConfigProperty();
         property.setName(LOWER_CASE);
         property.setLabel("To lowercase");
         property.setHelpText("Transform the attribute value to lowercase.");
         property.setType(ProviderConfigProperty.BOOLEAN_TYPE);
         property.setDefaultValue(false);
-
         configProperties.add(property);
+
         property = new ProviderConfigProperty();
         property.setName(ATTRIBUTE);
         property.setLabel("User Attribute Name");
@@ -95,31 +90,36 @@ public class PrefixAttributeMapper extends AbstractClaimMapper {
     }
 
     @Override
-    public void init(Config.Scope config) {
-        this.claim = config.get(CLAIM);
-        this.userAttribute = config.get(ATTRIBUTE);
-        this.prefix = config.get(PREFIX);
-        this.toLowerCase = config.getBoolean(LOWER_CASE, false);
-    }
-
-    @Override
     public void importNewUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
-        templateClaim(session, user, context);
+        templateClaim(session, user, mapperModel, context);
     }
 
     @Override
     public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
-        templateClaim(session, user, context);
+        templateClaim(session, user, mapperModel, context);
     }
 
-    private void templateClaim(KeycloakSession session, UserModel user, BrokeredIdentityContext context) {
+    private void templateClaim(KeycloakSession session, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        var config = mapperModel.getConfig();
+        String claim = config.get(CLAIM);
+        String userAttribute = config.get(ATTRIBUTE);
+        String prefix = config.get(PREFIX);
+        boolean toLowerCase = Boolean.parseBoolean(config.getOrDefault(LOWER_CASE, Boolean.FALSE.toString()));
+        logger.info("Claim: " + claim);
         Object claimValue = getClaimValue(context, claim);
-        if (claimValue instanceof Map) {
+        if (claimValue instanceof List) {
             logger.info(claimValue.toString());
-            ((Map<?, ?>) claimValue).forEach((k, v) -> logger.info(k + " is of type " + v.getClass()));
+            var claimValues = (List<Object>) claimValue;
+            claimValues.forEach(v -> logger.info(v + " is of type " + v.getClass()));
+            var prefixed = claimValues.stream()
+                    .map(c -> prefix + c)
+                    .map(toLowerCase ? String::toLowerCase : c -> c)
+                    .collect(Collectors.toList());
+            user.setAttribute(userAttribute, prefixed);
         } else {
-            logger.info(claimValue + " is of type "+ claimValue.getClass());
-            user.setSingleAttribute(userAttribute, prefix + claimValue);
+            logger.info(claimValue + " is of type " + claimValue.getClass());
+            String prefixed = prefix + claimValue;
+            user.setSingleAttribute(userAttribute, toLowerCase ? prefixed.toLowerCase() : prefixed);
         }
     }
 }
