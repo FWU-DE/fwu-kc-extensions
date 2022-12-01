94171239291c54b79ff5d7e892a06ae6c6e9ac53
mkunz-intension <michael.kunz@intension.de>
2022-12-01 13:59:05 +0100

    FWUV-65 - error handling improved

--- user-info-requester/src/main/java/de/intension/rest/RestClient.java
+++ user-info-requester/src/main/java/de/intension/rest/RestClient.java
@@ -16,6 +16,9 @@ public class RestClient
     {
     }
 
+    /**
+     * Performs a HTTP-GET request with the given bearer token.
+     */
     public static String get(URL url, String accessToken)
         throws IOException
     {
@@ -28,19 +31,46 @@ public class RestClient
         con.setReadTimeout(30 * 1000);
         con.setInstanceFollowRedirects(false);
 
-        logger.infof("Connection - response code: %s with message %s", con.getResponseCode(), con.getResponseMessage());
-
-        BufferedReader in = new BufferedReader(
+        if(isConnected(con)){
+            BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder builder = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            builder.append(inputLine);
-        }
-        in.close();
-        con.disconnect();
+            String inputLine;
+            StringBuilder builder = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                builder.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
 
-        return builder.toString();
+            return builder.toString();
+        } else {
+            return null;
+        }
+    }
+
+    private static boolean isConnected(HttpURLConnection con)
+        throws IOException
+    {
+        int httpCode = con.getResponseCode();
+        boolean connected = true;
+        if(httpCode >= HttpURLConnection.HTTP_BAD_REQUEST) {
+            connected = false;
+            StringBuilder builder = new StringBuilder();
+            try{
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    builder.append(line);
+                }
+                in.close();
+            } catch (IOException e){
+                //do nothing
+            }
+            logger.errorf("Connection failed: %s - response code: %s with message %s", con.getURL().toString(), httpCode, builder.toString());
+        } else {
+            logger.infof("Connection established: %s - response code: %s", con.getURL().toString(), httpCode);
+        }
+        return connected;
     }
 
 }
