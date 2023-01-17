d9df9a7721b4ca73040f849f5e1c81a443ac32f3
Ingo Kuba <ikuba@intension.de>
2023-01-17 18:02:28 +0100

    intellij format removed

--- hmac-mapper/src/main/java/de/intension/protocol/oidc/mappers/IPairwiseMapper.java
+++ hmac-mapper/src/main/java/de/intension/protocol/oidc/mappers/IPairwiseMapper.java
@@ -1,16 +1,21 @@
 package de.intension.protocol.oidc.mappers;
 
-import org.keycloak.models.*;
+import java.util.List;
+
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ProtocolMapperContainerModel;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.RealmModel;
 import org.keycloak.protocol.ProtocolMapperConfigException;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
 
-import java.util.List;
-
 /**
- * Interface to "adopt" the {@link org.keycloak.protocol.oidc.mappers.AbstractPairwiseSubMapper}
+ * Interface to "adopt" the
+ * {@link org.keycloak.protocol.oidc.mappers.AbstractPairwiseSubMapper}
  * behaviour.
  * Should make it easier to identify code changes after a new Keycloak release.
  */
@@ -26,17 +31,20 @@ public interface IPairwiseMapper
 
     /**
      * Implement to add additional provider configuration properties.
-     * By default, a pairwise sub mapper will only contain configuration for a sector identifier
+     * By default, a pairwise sub mapper will only contain configuration for a
+     * sector identifier
      * URI.
      */
     List<ProviderConfigProperty> getAdditionalConfigProperties();
 
     /**
      * Implement to add additional configuration validation.
-     * Called when instance of mapperModel is created/updated for this protocolMapper through admin
+     * Called when instance of mapperModel is created/updated for this
+     * protocolMapper through admin
      * endpoint.
      */
-    void validateAdditionalConfig(KeycloakSession session, RealmModel realm, ProtocolMapperContainerModel mapperContainer, ProtocolMapperModel mapperModel)
+    void validateAdditionalConfig(KeycloakSession session, RealmModel realm,
+            ProtocolMapperContainerModel mapperContainer, ProtocolMapperModel mapperModel)
             throws ProtocolMapperConfigException;
 
     /**
