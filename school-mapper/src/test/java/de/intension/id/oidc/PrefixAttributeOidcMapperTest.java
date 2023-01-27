39fd2e1512570e0cfa1308fc42e0800162145b15
Ingo Kuba <ikuba@intension.de>
2023-01-27 16:05:24 +0100

    double oops

--- school-mapper/src/test/java/de/intension/id/oidc/PrefixAttributeOidcMapperTest.java
+++ school-mapper/src/test/java/de/intension/id/oidc/PrefixAttributeOidcMapperTest.java
@@ -1,5 +1,16 @@
 package de.intension.id.oidc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.times;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,16 +25,10 @@ import org.keycloak.representations.JsonWebToken;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import de.intension.id.PrefixAttributeConstants;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.times;
-
-class PrefixAttributeOidcMapperTest {
+class PrefixAttributeOidcMapperTest
+{
 
     @ParameterizedTest
     @CsvSource({
@@ -31,7 +36,8 @@ class PrefixAttributeOidcMapperTest {
             "false,PREFIX.1234",
             "true,prefix.1234"
     })
-    void should_map_school_id_with_prefix(Boolean lowercase, String expected) {
+    void should_map_school_id_with_prefix(Boolean lowercase, String expected)
+    {
         ArgumentCaptor<String> prefixCaptor = ArgumentCaptor.forClass(String.class);
         var token = token("schoolId", "1234");
         var config = mapperConfig("schoolId", "prefixedId", "PREFIX.", lowercase);
@@ -52,7 +58,8 @@ class PrefixAttributeOidcMapperTest {
             "1234##,false,1,PREFIX.1234"
     })
     @SuppressWarnings("unchecked")
-    void should_map_multiple_school_ids_with_prefix(String claimValue, Boolean lowercase, Integer expectedAmount, String expected) {
+    void should_map_multiple_school_ids_with_prefix(String claimValue, Boolean lowercase, Integer expectedAmount, String expected)
+    {
         ArgumentCaptor<List<String>> prefixCaptor = ArgumentCaptor.forClass(List.class);
         var token = token("schoolId", Arrays.asList(claimValue.split("##")));
         var config = mapperConfig("schoolId", "prefixedId", "PREFIX.", lowercase);
@@ -66,7 +73,8 @@ class PrefixAttributeOidcMapperTest {
     }
 
     @Test
-    void should_not_map_school_id_if_token_misses_claim() {
+    void should_not_map_school_id_if_token_misses_claim()
+    {
         var token = new JsonWebToken();
         var config = mapperConfig("schoolId", "prefixedId", "NOT ", true);
 
@@ -78,7 +86,8 @@ class PrefixAttributeOidcMapperTest {
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"   "})
-    void should_not_map_school_id_if_claim_value_is_invalid(String value) {
+    void should_not_map_school_id_if_claim_value_is_invalid(String value)
+    {
         var token = token("school", value);
         var config = mapperConfig("school", "prefixed", "NOT ", true);
 
@@ -88,7 +97,8 @@ class PrefixAttributeOidcMapperTest {
     }
 
     @Test
-    void should_not_map_school_ids_if_claim_value_is_empty_list() {
+    void should_not_map_school_ids_if_claim_value_is_empty_list()
+    {
         var token = token("school", List.of());
         var config = mapperConfig("school", "prefixed", "NOT ", true);
 
@@ -97,23 +107,26 @@ class PrefixAttributeOidcMapperTest {
         Mockito.verifyNoInteractions(user);
     }
 
-    private Map<String, String> mapperConfig(String claim, String attribute, String prefix, Boolean lowercase) {
+    private Map<String, String> mapperConfig(String claim, String attribute, String prefix, Boolean lowercase)
+    {
         var config = new HashMap<>(Map.of(AbstractClaimMapper.CLAIM, claim,
-                PrefixAttributeOidcMapper.ATTRIBUTE, attribute,
-                PrefixAttributeOidcMapper.PREFIX, prefix));
+                                          PrefixAttributeOidcMapper.ATTRIBUTE, attribute,
+                                          PrefixAttributeConstants.PREFIX, prefix));
         if (lowercase != null) {
-            config.put(PrefixAttributeOidcMapper.LOWER_CASE, Boolean.toString(lowercase));
+            config.put(PrefixAttributeConstants.LOWER_CASE, Boolean.toString(lowercase));
         }
         return config;
     }
 
-    private JsonWebToken token(String claim, Object value) {
+    private JsonWebToken token(String claim, Object value)
+    {
         var token = new JsonWebToken();
         token.setOtherClaims(claim, value);
         return token;
     }
 
-    private UserModel testMapping(JsonWebToken token, Map<String, String> mapperConfig) {
+    private UserModel testMapping(JsonWebToken token, Map<String, String> mapperConfig)
+    {
         var context = Mockito.mock(BrokeredIdentityContext.class);
         Mockito.when(context.getContextData()).thenReturn(Map.of(KeycloakOIDCIdentityProvider.VALIDATED_ACCESS_TOKEN, token));
         var mapperModel = Mockito.mock(IdentityProviderMapperModel.class);
