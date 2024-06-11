dc4055821f36206db4ab7d06e3e85b9fd3e3afb1
jhammel@intension.de <119927851+jens-hammel@users.noreply.github.com>
2024-06-11 09:30:02 +0200

    Fix deletionquery and refactor/add tests (#85)

--- remove-user-on-logout/src/test/java/de/intension/listener/RemoveAllTypesOnLogOutEventIT.java
+++ remove-user-on-logout/src/test/java/de/intension/listener/RemoveAllTypesOnLogOutEventIT.java
@@ -7,14 +7,12 @@ import java.net.URL;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
+import de.intension.testhelper.KeycloakPage;
 import org.junit.jupiter.api.*;
 import org.keycloak.admin.client.resource.UsersResource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
@@ -120,61 +118,6 @@ class RemoveAllTypesOnLogOutEventIT
     void cleanUp()
     {
         driver.quit();
-    }
-
-    /**
-     * Container for handling interactions with the Keycloak web pages.
-     */
-    private static class KeycloakPage
-    {
-
-        private final WebDriver             driver;
-        private final FluentWait<WebDriver> wait;
-
-        private KeycloakPage(WebDriver driver, FluentWait<WebDriver> wait)
-        {
-            this.driver = driver;
-            this.wait = wait;
-        }
-
-        static KeycloakPage start(WebDriver driver, FluentWait<WebDriver> wait)
-        {
-            return new KeycloakPage(driver, wait);
-        }
-
-        public KeycloakPage openAccountConsole()
-        {
-            driver.get("http://test:8080/auth/realms/fwu/account/#/personal-info");
-            return this;
-        }
-
-        public KeycloakPage idpLogin(String username, String password)
-        {
-            wait.until(ExpectedConditions.elementToBeClickable(By.id("social-keycloak-oidc")));
-            driver.findElement(By.id("social-keycloak-oidc")).click();
-            return login(username, password);
-        }
-
-        private KeycloakPage login(String username, String password)
-        {
-            By usernameInput = By.cssSelector("input#username");
-            wait.until(ExpectedConditions.elementToBeClickable(By.id("kc-login")));
-            wait.until(ExpectedConditions.presenceOfElementLocated(usernameInput));
-            driver.findElement(usernameInput).sendKeys(username);
-            driver.findElement(By.cssSelector("input#password")).sendKeys(password);
-            driver.findElement(By.cssSelector("input#kc-login")).click();
-            return this;
-        }
-
-        public KeycloakPage logout()
-        {
-            By signOutButton = By.xpath("//button[text()='Sign out']");
-            ((RemoteWebDriver)driver).getScreenshotAs(OutputType.FILE);
-            wait.until(ExpectedConditions.elementToBeClickable(signOutButton));
-            driver.findElement(signOutButton).click();
-            return this;
-        }
-
     }
 
 }
