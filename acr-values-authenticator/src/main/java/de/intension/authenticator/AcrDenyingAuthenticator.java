b3104171dd3f8c5f1ff319f52d21f5e4f596f840
Ingo Kuba <ikuba@intension.de>
2025-01-14 10:53:25 +0100

    change

--- acr-values-authenticator/src/main/java/de/intension/authenticator/AcrDenyingAuthenticator.java
+++ acr-values-authenticator/src/main/java/de/intension/authenticator/AcrDenyingAuthenticator.java
@@ -1,18 +1,16 @@
 package de.intension.authenticator;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
-import org.keycloak.models.*;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.protocol.oidc.utils.AcrUtils;
 
 import java.util.Map;
-
-import static de.intension.authenticator.AcrDenyingAuthenticatorFactory.CONF_LOA_KEY;
-import static org.keycloak.models.Constants.ACR_LOA_MAP;
 
 /**
  * Authenticator for post login flow which denies if user attribute does not have attribute matching the LoA settings of current client.
@@ -32,13 +30,12 @@ public class AcrDenyingAuthenticator implements Authenticator {
     public void authenticate(AuthenticationFlowContext context) {
         try {
             var client = session.getContext().getClient();
-            var loaKey = getLoaMapKey(context);
-            Map<String, String> loaMap = getAcrLoaMap(client);
-            if (loaMap.containsKey(loaKey)) {
-                var acrValue = loaMap.get(loaKey);
-                var hasAcrAttribute = context.getUser().getAttributeStream(loaKey).anyMatch(acrValue::equals);
+            // get ACR to LOA mapping from client or realm
+            Map<String, Integer> acrLoaMap = AcrUtils.getAcrLoaMap(client);
+            if (!acrLoaMap.isEmpty()) {
+                var hasAcrAttribute = context.getUser().getAttributeStream("acr_values").anyMatch(acrLoaMap::containsKey);
                 if (!hasAcrAttribute) {
-                    logger.warnf("Denied request due to ACR in user attribute not matching client configuration. Realm %s, client %s, user %s.",
+                    logger.warnf("Denied request due to acr_values in user attribute not matching client configuration. Realm %s, client %s, user %s.",
                             context.getRealm().getName(),
                             client.getClientId(),
                             context.getUser().getId());
@@ -52,26 +49,6 @@ public class AcrDenyingAuthenticator implements Authenticator {
             return;
         }
         context.success();
-    }
-
-    private String getLoaMapKey(AuthenticationFlowContext context) {
-        final String defaultKey = "mfa";
-        AuthenticatorConfigModel authenticatorConfig = context.getAuthenticatorConfig();
-        if (authenticatorConfig == null) {
-            return defaultKey;
-        }
-        return authenticatorConfig.getConfig().getOrDefault(CONF_LOA_KEY, defaultKey);
-    }
-
-    private Map<String, String> getAcrLoaMap(ClientModel client) throws JsonProcessingException {
-        TypeReference<Map<String, String>> typeRef = new TypeReference<>() {
-        };
-        String acrLoaMap = client.getAttribute(ACR_LOA_MAP);
-        if (acrLoaMap == null) {
-            logger.warnf("Client '%s' does not have attribute '%s' set.", client.getClientId(), ACR_LOA_MAP);
-            return Map.of();
-        }
-        return mapper.readValue(acrLoaMap, typeRef);
     }
 
     @Override
