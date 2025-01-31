b620d7d4b8a9d8980e8e5e65cfd3b6722e6e0426
Ingo Kuba <ikuba@intension.de>
2025-01-31 09:26:11 +0100

    mash

--- user-licence-lifecycle-manager/src/main/java/de/intension/rest/LicenceResource.java
+++ user-licence-lifecycle-manager/src/main/java/de/intension/rest/LicenceResource.java
@@ -6,17 +6,27 @@ import jakarta.ws.rs.core.MediaType;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.models.KeycloakSession;
 
+/**
+ * Custom rest endpoints exposed via <code>${authUrl}/realms/${realm}/licences-resource/</code>.
+ */
 @RequiredArgsConstructor
 public class LicenceResource {
 
     private final KeycloakSession session;
 
+    /**
+     * Get licences via their ID, which is the user's ID hashed with HMAC.
+     *
+     * <pre>
+     *     GET ${authUrl}/realms/${realm}/licences-resource/${hmacId}
+     * </pre>
+     */
     @Path("/{hmac-id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getLicence(@PathParam("hmac-id") String hmacID) {
-        var licence =  session.getProvider(LicenceJpaProvider.class).getLicenceByHmacId(hmacID);
-        if(licence == null) {
+        var licence = session.getProvider(LicenceJpaProvider.class).getLicenceByHmacId(hmacID);
+        if (licence == null) {
             throw new NotFoundException("No licence with hmac-id " + hmacID + " found.");
         }
         return licence;
--- user-licence-lifecycle-manager/src/main/java/de/intension/rest/LicenceResourceProviderFactory.java
+++ user-licence-lifecycle-manager/src/main/java/de/intension/rest/LicenceResourceProviderFactory.java
@@ -1,16 +1,28 @@
 package de.intension.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.services.resource.RealmResourceProvider;
 import org.keycloak.services.resource.RealmResourceProviderFactory;
 
-public class LicenceResourceProviderFactory implements RealmResourceProviderFactory {
+/**
+ * Provider and provider factory for {@link LicenceResource}.
+ */
+@RequiredArgsConstructor
+public class LicenceResourceProviderFactory implements RealmResourceProvider, RealmResourceProviderFactory {
+
+    private final KeycloakSession session;
 
     @Override
     public RealmResourceProvider create(KeycloakSession keycloakSession) {
-        return new LicenceResourceProvider(keycloakSession);
+        return this;
+    }
+
+    @Override
+    public Object getResource() {
+        return new LicenceResource(session);
     }
 
     @Override
