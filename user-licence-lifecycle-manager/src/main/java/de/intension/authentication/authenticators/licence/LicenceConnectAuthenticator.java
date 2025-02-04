b95a2329a9551a01e993290a33ad681fd32f8d05
Ingo Kuba <ikuba@intension.de>
2025-02-04 16:51:42 +0100

    FWUV-236: Fallback to "licenses" if "licences" is not returned by Licence Connect

--- user-licence-lifecycle-manager/src/main/java/de/intension/authentication/authenticators/licence/LicenceConnectAuthenticator.java
+++ user-licence-lifecycle-manager/src/main/java/de/intension/authentication/authenticators/licence/LicenceConnectAuthenticator.java
@@ -59,6 +59,10 @@ public class LicenceConnectAuthenticator
         JsonNode userLicences = fetchUserLicence(licenceRequest);
         if (userLicences != null) {
             String userLicence = userLicences.path(LICENCE_ATTRIBUTE).toString();
+            if (userLicence == null || userLicence.isBlank()) {
+                // fallback to American licences
+                userLicence = userLicences.path("licenses").toString();
+            }
             for (int i = 0; i < userLicence.length(); i += PART_SIZE) {
                 String partValue = userLicence.substring(i, Math.min(userLicence.length(), i + PART_SIZE));
                 user.setAttribute(LICENCE_ATTRIBUTE + (i / PART_SIZE + 1), List.of(partValue));
